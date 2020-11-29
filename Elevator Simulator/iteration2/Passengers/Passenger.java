c657298c91f9660813b0c427bb45627e167b11aa
Issa Issa <48043653+iissa3339@users.noreply.github.com>
2020-11-29 12:56:11 -0800

    Create Passenger.java

---
+++ Elevator Simulator/iteration2/Passengers/Passenger.java
@@ -0,0 +1,161 @@
+package cecs277.passengers;
+
+import cecs277.buildings.Floor;
+import cecs277.buildings.FloorObserver;
+import cecs277.elevators.Elevator;
+import cecs277.elevators.ElevatorObserver;
+
+/**
+ * A passenger that is either waiting on a floor or riding an elevator.
+ */
+public abstract class Passenger implements FloorObserver, ElevatorObserver {
+	// An enum for determining whether a Passenger is on a floor, an elevator, or busy (visiting a room in the building).
+	public enum PassengerState {
+		WAITING_ON_FLOOR,
+		ON_ELEVATOR,
+		BUSY
+	}
+	
+	// A cute trick for assigning unique IDs to each object that is created. (See the constructor.)
+	private static int mNextId;
+	protected static int nextPassengerId() {
+		return ++mNextId;
+	}
+	
+	private int mIdentifier;
+	private PassengerState mCurrentState;
+	
+	public Passenger() {
+		mIdentifier = nextPassengerId();
+		mCurrentState = PassengerState.WAITING_ON_FLOOR;
+	}
+	
+	public void setState(PassengerState state) {
+		mCurrentState = state;
+	}
+	
+	public PassengerState getState() {
+		return mCurrentState;
+	}
+	
+	/**
+	 * Gets the passenger's unique identifier.
+	 */
+	public int getId() {
+		return mIdentifier;
+	}
+	
+	/**
+	 * Handles an elevator arriving at the passenger's current floor.
+	 */
+	@Override
+	public void elevatorArriving(Floor floor, Elevator elevator) {
+		// This is a sanity check. A Passenger should never be observing a Floor they are not waiting on.
+		if (floor.getWaitingPassengers().contains(this) && mCurrentState == PassengerState.WAITING_ON_FLOOR) {
+			Elevator.Direction elevatorDirection = elevator.getCurrentDirection();
+					
+			// TODO: check if the elevator is either NOT_MOVING, or is going in the direction that this passenger wants.
+			// If so, this passenger becomes an observer of the elevator.
+			
+			if(elevatorDirection == Elevator.Direction.NOT_MOVING || 
+					(getDestination() > elevator.getCurrentFloor().getNumber() && elevatorDirection == Elevator.Direction.MOVING_UP)||
+					(getDestination() < elevator.getCurrentFloor().getNumber() && elevatorDirection == Elevator.Direction.MOVING_DOWN)) {
+				elevator.addObserver(this);
+			}
+		}
+		// This else should not happen if your code is correct. Do not remove this branch; it reveals errors in your code.
+		else {
+			throw new RuntimeException("Passenger " + toString() + " is observing Floor " + floor.getNumber() + " but they are " +
+			 "not waiting on that floor.");
+		}
+	}
+	
+	/**
+	 * Handles an observed elevator opening its doors. Depart the elevator if we are on it; otherwise, enter the elevator.
+	 */
+	@Override
+	public void elevatorDoorsOpened(Elevator elevator) {
+		// TODO: The elevator is arriving at our destination. Remove ourselves from the elevator, and stop observing it.
+		// TODO: Does NOT handle any "next" destination...
+		if (mCurrentState == PassengerState.ON_ELEVATOR && elevator.getCurrentFloor().getNumber() == getDestination()) {
+			// TODO: remove this passenger from the elevator, and as an observer of the elevator. Call the
+			// leavingElevator method to allow a derived class to do something when the passenger departs.
+			// Set the current state to BUSY.
+			elevator.removePassenger(this);
+			elevator.removeObserver(this);
+			leavingElevator(elevator);
+			setState(PassengerState.BUSY);
+		}
+		// The elevator has arrived on the floor we are waiting on. If the elevator has room for us, remove ourselves
+		// from the floor, and enter the elevator.
+		else if (mCurrentState == PassengerState.WAITING_ON_FLOOR) {
+			// TODO: determine if the passenger will board the elevator using willBoardElevator.
+			// If so, remove the passenger from the current floor, and as an observer of the current floor;
+			// then add the passenger as an observer of and passenger on the elevator. Then set the mCurrentState
+			// to ON_ELEVATOR.
+	
+			Floor passFloor = null;
+			for(int i = 1; i <= elevator.getBuilding().getFloorCount(); i++) {
+				Floor x = elevator.getBuilding().getFloor(i);
+				if(x.getWaitingPassengers().contains(this)) {
+					passFloor = x;
+					break;
+				}
+			}
+			if(willBoardElevator(elevator) && elevator.getCurrentFloor() == passFloor) {
+				elevator.getCurrentFloor().removeWaitingPassenger(this);
+				elevator.getCurrentFloor().removeObserver(this);
+				elevator.addPassenger(this);
+				elevator.addObserver(this);
+				setState(PassengerState.ON_ELEVATOR);
+			}
+		}
+	}
+	
+	/**
+	 * Returns the passenger's current destination (what floor they are traveling to).
+	 */
+	public abstract int getDestination();
+	
+	/**
+	 * Called to determine whether the passenger will board the given elevator that is moving in the direction the
+	 * passenger wants to travel.
+	 */
+	protected abstract boolean willBoardElevator(Elevator elevator);
+	
+	/**
+	 * Called when the passenger is departing the given elevator.
+	 */
+	protected abstract void leavingElevator(Elevator elevator);
+	
+	// This will be overridden by derived types.
+	@Override
+	public String toString() {
+		return Integer.toString(getDestination());
+	}
+	
+	@Override
+	public void directionRequested(Floor sender, Elevator.Direction direction) {
+		// Don't care.
+	}
+	
+	@Override
+	public void elevatorWentIdle(Elevator elevator) {
+		// Don't care about this.
+	}
+	
+	// The next two methods allow Passengers to be used in data structures, using their id for equality. Don't change 'em.
+	@Override
+	public int hashCode() {
+		return Integer.hashCode(mIdentifier);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Passenger passenger = (Passenger)o;
+		return mIdentifier == passenger.mIdentifier;
+	}
+	
+}
